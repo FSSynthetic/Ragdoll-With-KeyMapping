1e3bdbf4b1d1c9c6898b3bc292ad3b46e2ec07c1
FSSynthetic <fssynthetic.dev@gmail.com>
2020-11-27 18:50:12 -0500

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,1 @@
+client_script "ragdoll-c.lua"
---
+++ ragdoll-c.lua
@@ -0,0 +1,51 @@
+-----------------------------------
+-- Toogle Ragdoll, Made by JAF ----
+--------- ReConfigured by ---------
+---- FSSynthetic & Campinchris ----
+-----------------------------------
+
+---------------------------------------------------
+--Local Variables
+---------------------------------------------------
+local ragdoll = false
+ragdol = true
+
+---------------------------------------------------
+--Key Mapping Register Shit
+---------------------------------------------------
+RegisterCommand('rdoll', function()
+    TriggerEvent("Ragdoll", source)
+end, false)
+TriggerEvent( "chat:addSuggestion", "/rdoll", "Puts you in ragdoll until you click a key of your choice to set as the permanent ragdoll key. (Can be changed later in Setting/Key Bindings/FiveM)" )
+
+RegisterKeyMapping('rdoll', 'Toggle Ragdoll', 'keyboard', 'x') -- ('(Chat Command)', '(Keybind Menu Description)', '(Input Device)', '(Default Keymap)')
+
+---------------------------------------------------
+--Client Shit
+---------------------------------------------------
+Citizen.CreateThread(function()
+    while true do
+    Citizen.Wait(0)
+        if ragdoll then
+          SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, 0, 0, 0)
+        end
+    end
+end)
+    
+RegisterNetEvent("Ragdoll")
+AddEventHandler("Ragdoll", function()
+    if (ragdol) then
+        setRagdoll(true)
+        ragdol = false
+    else
+        setRagdoll(false)
+        ragdol = true
+    end
+end)
+
+---------------------------------------------------
+--Functions
+---------------------------------------------------
+function setRagdoll(rag)
+    ragdoll = rag
+end
