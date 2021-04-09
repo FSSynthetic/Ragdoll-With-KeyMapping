e783313ff8df61f748431c2f11535973728b716d
FSSynthetic <fssynthetic.dev@gmail.com>
2021-04-09 00:17:36 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'FSSynthetic'
-description 'Toggle ragdoll with custom key binding.'
+description 'Toggle ragdoll with a chat command and custom keybinding ability.'
 version '1.0.0'
 
 -- What to run
