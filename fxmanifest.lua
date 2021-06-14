91f7f1fe2dac6b1041f05c6dbbca7346ad279efa
FSSynthetic <fssynthetic.dev@gmail.com>
2021-06-13 22:33:03 -0400

    Delete fxmanifest.lua

--- fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'FSSynthetic'
-description 'Toggle ragdoll with a chat command and custom keybinding ability.'
-version '1.0.0'
-
--- What to run
-client_script 'ragdoll-c.lua'
